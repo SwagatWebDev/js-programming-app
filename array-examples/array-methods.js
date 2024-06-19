fd1e5764af38b34c1afd45ac1b5bca1cf9965a34
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-06-19 09:44:34 +0530

    Update array-methods.js

--- array-examples/array-methods.js
+++ array-examples/array-methods.js
@@ -31,6 +31,45 @@ const myArray = [1, 2, 3];
 const sum = myArray + 10;
 console.log(sum);
 
+ List<LicensePromptGuidelineDto> allGuidelines = new ArrayList<>();
+
+        if (technology.getOpensourceLicenses() != null) {
+            technology.getOpensourceLicenses().stream()
+                .filter(license -> license.getLicenses() != null)
+                .flatMap(license -> license.getLicenses().stream())
+                .forEach(allGuidelines::add);
+        }
+
+        if (technology.getPredecessorTechnology() != null) {
+            technology.getPredecessorTechnology().stream()
+                .flatMap(List::stream)
+                .forEach(allGuidelines::add);
+        }
+
+        if (technology.getSuccessorTechnology() != null) {
+            technology.getSuccessorTechnology().stream()
+                .flatMap(List::stream)
+                .forEach(allGuidelines::add);
+        }
+
+        String initRec = determineValue(allGuidelines, LicensePromptGuidelineDto::getInitRec);
+        String initPrompt = determineValue(allGuidelines, LicensePromptGuidelineDto::getInitPrompt);
+        String extRec = determineValue(allGuidelines, LicensePromptGuidelineDto::getExtRec);
+        String extPrompt = determineValue(allGuidelines, LicensePromptGuidelineDto::getExtPrompt);
+
+        return String.format("initRec: %s, initPrompt: %s, extRec: %s, extPrompt: %s", initRec, initPrompt, extRec, extPrompt);
+    }
+
+    private static String determineValue(List<LicensePromptGuidelineDto> guidelines, Function<LicensePromptGuidelineDto, String> getter) {
+        return guidelines.stream()
+            .map(getter)
+            .filter(Objects::nonNull)
+            .distinct()
+            .sorted((a, b) -> "restriction".equals(b) ? 1 : -1)
+            .findFirst()
+            .orElse("available");  // Default value if no guidelines are found
+    }
+
 //fill
 const num = [1, 2, 3, 4, 5];
 num.fill(0, 1, 4);
