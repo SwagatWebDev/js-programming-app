dbafd041cec77dc6b19a2ec0b526ce81f619009a
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-06-26 00:03:24 +0530

    Update array-methods.js

--- array-examples/array-methods.js
+++ array-examples/array-methods.js
@@ -26,7 +26,65 @@ const primitiveValue = colors.valueOf();
 console.log(primitiveValue);
 
 console.log(primitiveValue === colors);
+ List<LLMOpenSourceTechnology> technologies = Arrays.asList(
+                // Add your sample data here
+        );
 
+        technologies.forEach(LLMOpenSourceTechnologyPromptExtractor::extractPromptGuidelines);
+    }
+
+    public static void extractPromptGuidelines(LLMOpenSourceTechnology tech) {
+        tech.overViewIntPrompt = getWorstCaseValue(tech, "intPrompt");
+        tech.overviewExtPrompt = getWorstCaseValue(tech, "extPrompt");
+        tech.overviewIntRecipient = getWorstCaseValue(tech, "intRecipient");
+        tech.overviewExtRecipient = getWorstCaseValue(tech, "extRecipient");
+    }
+
+    private static String getWorstCaseValue(LLMOpenSourceTechnology tech, String fieldName) {
+        Stream<String> currentTechnologyValues = Stream.of(
+                getValueFromField(tech.intPrompt, fieldName),
+                getValueFromField(tech.extPrompt, fieldName),
+                getValueFromField(tech.intRecipient, fieldName),
+                getValueFromField(tech.extRecipient, fieldName)
+        ).filter(Objects::nonNull);
+
+        Stream<String> openSourceLicenseValues = tech.openSourceLicense.stream()
+                .flatMap(osl -> osl.licenses.stream())
+                .flatMap(license -> Stream.of(
+                        getValueFromField(license.intPrompt, fieldName),
+                        getValueFromField(license.extPrompt, fieldName),
+                        getValueFromField(license.intRecipient, fieldName),
+                        getValueFromField(license.extRecipient, fieldName)
+                ))
+                .filter(Objects::nonNull);
+
+        Stream<String> predecessorTechnologyValues = tech.predecessorTechnology.stream()
+                .flatMap(predecessor -> Stream.concat(
+                        Stream.of(
+                                getValueFromField(predecessor.intPrompt, fieldName),
+                                getValueFromField(predecessor.extPrompt, fieldName),
+                                getValueFromField(predecessor.intRecipient, fieldName),
+                                getValueFromField(predecessor.extRecipient, fieldName)
+                        ).filter(Objects::nonNull),
+                        predecessor.licenses.stream()
+                                .filter(license -> license.isComplianceGeneration || license.isComplianceModification)
+                                .flatMap(license -> Stream.of(
+                                        getValueFromField(license.intPrompt, fieldName),
+                                        getValueFromField(license.extPrompt, fieldName),
+                                        getValueFromField(license.intRecipient, fieldName),
+                                        getValueFromField(license.extRecipient, fieldName)
+                                )).filter(Objects::nonNull)
+                ));
+
+        return Stream.of(currentTechnologyValues, openSourceLicenseValues, predecessorTechnologyValues)
+                .flatMap(s -> s)
+                .min(Comparator.comparingInt(PRIORITY::indexOf))
+                .orElse("noRestrictionIdentified");
+    }
+
+    private static String getValueFromField(String value, String fieldName) {
+        return value;
+    }
 const myArray = [1, 2, 3];
 const sum = myArray + 10;
 console.log(package com.fmr.intelea.batch.service;
