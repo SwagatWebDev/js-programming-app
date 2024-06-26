e82b8530d7a2e096ea643d2c98570038197851bf
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-06-26 07:56:06 +0530

    Update test.js

--- basic-javascript/test.js
+++ basic-javascript/test.js
@@ -1,6 +1,1 @@
-List<License> allLicenses = Stream.concat(
-                tech.openSourceLicense != null ? tech.openSourceLicense.stream().flatMap(osLicense -> osLicense.Licenses.stream()) : Stream.empty(),
-                (tech.openSourceLicense == null || tech.openSourceLicense.isEmpty()) && tech.predecessorTechnology != null
-                        ? tech.predecessorTechnology.stream().flatMap(predecessor -> predecessor.Licenses.stream())
-                        : Stream.empty()
-        ).collect(Collectors.toList());
+
