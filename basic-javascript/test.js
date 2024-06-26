945c0c5bb87f0d761ecbdc0f8d376fb834ec0652
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-06-26 07:51:25 +0530

    Update test.js

--- basic-javascript/test.js
+++ basic-javascript/test.js
@@ -1,1 +1,10 @@
-
+private static String findWorstCase(Stream<String> values) {
+        return values.filter(value -> value != null)
+                .min(Comparator.comparingInt(value -> switch (value) {
+                    case "alwaysReject" -> 1;
+                    case "alwaysReviewed" -> 2;
+                    case "alwaysApproved" -> 3;
+                    default -> Integer.MAX_VALUE;
+                }))
+                .orElse(null);
+    }
