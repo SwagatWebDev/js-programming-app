ef8d8d606187942acffcf5943e547f705d043c04
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-10-05 18:25:45 +0530

    Update spread.js

--- es-6/spread.js
+++ es-6/spread.js
@@ -26,25 +26,3 @@ console.log(clonedObject);
 const originalArray = [{id: 1}, {id: 2}]
 const copiedArray = [...originalArray];
 console.log(copiedArray);
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<Object> handleResponseStatusException(ResponseStatusException ex) {
-        Map<String, Object> errorDetails = new HashMap<>();
-        errorDetails.put("message", ex.getReason());
-        errorDetails.put("status", ex.getStatus().value());
-        return new ResponseEntity<>(errorDetails, ex.getStatus());
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception ex) {
-        Map<String, Object> errorDetails = new HashMap<>();
-        errorDetails.put("message", "Internal Server Error: " + ex.getMessage());
-        errorDetails.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
-
-
