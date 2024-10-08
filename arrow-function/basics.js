3e27c5dcef05a0beebc4892e93225b6022f148a3
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-10-08 23:54:51 +0530

    Update basics.js

--- arrow-function/basics.js
+++ arrow-function/basics.js
@@ -35,4 +35,79 @@ const greet = (name = 'Guest') => {
 };
 greet();
 greet('Alice'); 
+import * as Highcharts from 'highcharts';
+
+describe('Highcharts Chart Event Tests', () => {
+  let chart: any;
+  let chartOptions: any;
+
+  beforeEach(() => {
+    // Mock Highcharts functions and chart object
+    chartOptions = {
+      chart: {
+        type: 'bar',
+        backgroundColor: '#F9F9FB',
+        marginTop: 100,
+        events: {
+          load: function () {
+            const chart = this;
+            Highcharts.addEvent(chart.container, 'mouseleave', function () {
+              chart.tooltip.hide();
+            });
+            Highcharts.addEvent(chart.container, 'mousemove', function () {
+              const point = chart.hoverPoint;
+              if (!point) {
+                chart.tooltip.hide();
+              }
+            });
+          }
+        }
+      }
+    };
+
+    // Mock chart object with the necessary properties
+    chart = {
+      container: document.createElement('div'),
+      tooltip: {
+        hide: jasmine.createSpy('hide')
+      },
+      hoverPoint: null
+    };
+
+    // Simulate the load event handler to attach mouse events
+    chartOptions.chart.events.load.call(chart);
+  });
+
+  it('should hide the tooltip on mouseleave', () => {
+    // Trigger the mouseleave event
+    const mouseLeaveEvent = new Event('mouseleave');
+    chart.container.dispatchEvent(mouseLeaveEvent);
+
+    // Assert that the tooltip hide function was called
+    expect(chart.tooltip.hide).toHaveBeenCalled();
+  });
+
+  it('should hide the tooltip on mousemove when no point is hovered', () => {
+    // Trigger the mousemove event without a hoverPoint
+    const mouseMoveEvent = new Event('mousemove');
+    chart.container.dispatchEvent(mouseMoveEvent);
+
+    // Assert that the tooltip hide function was called
+    expect(chart.tooltip.hide).toHaveBeenCalled();
+  });
+
+  it('should not hide the tooltip on mousemove when a point is hovered', () => {
+    // Set a hoverPoint
+    chart.hoverPoint = { some: 'point' };
+
+    // Trigger the mousemove event with a hoverPoint
+    const mouseMoveEvent = new Event('mousemove');
+    chart.container.dispatchEvent(mouseMoveEvent);
+
+    // Assert that the tooltip hide function was not called
+    expect(chart.tooltip.hide).not.toHaveBeenCalled();
+  });
+});
+
+
             
