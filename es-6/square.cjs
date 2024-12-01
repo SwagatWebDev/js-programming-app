07d19e8bf9883a11229ff51ea46b71efef071fa7
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-12-01 12:01:44 +0530

    Update square.cjs

--- es-6/square.cjs
+++ es-6/square.cjs
@@ -7,13 +7,3 @@
 // const square = new Square(4);
 // console.log(square.area());
 
-
- <div style="display: flex; justify-content: space-between; align-items: center; width: 100%;">
-      <span style="font-size: 16px; font-family: Montserrat, sans-serif !important; font-weight: 700;">
-        Solutions Disposition Distribution
-      </span>
-      <span style="font-size: 16px; font-family: Montserrat, sans-serif !important; font-weight: 700;">
-        100
-      </span>
-    </div>
-
