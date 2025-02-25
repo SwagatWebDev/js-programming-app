68d921fbdba0c2eea5ea1031ab57d7dd7fb0b7f1
Swagat Mishra <swagatkumarmishra888@gmail.com>
2025-02-26 04:03:07 +0530

    Update promise-race-example.js

--- promise/promise-race-example.js
+++ promise/promise-race-example.js
@@ -19,26 +19,3 @@ Promise.race([
 }).catch((error) => {
     console.log('An error occurred', error);
 })
-if (ObjectUtils.isNotEmpty(filter.getProvider())) {
-    List<String> providerList = switch (filter.getProvider().toUpperCase()) {
-        case "OTHER" -> CommonConstants.OTHER_BU;
-        case "ETGS" -> CommonConstants.ETGS_BU;
-        case "FB/FWM" -> CommonConstants.FB_FWM_BU;
-        case "FBT/FIT" -> CommonConstants.FBT_FIT_BU;
-        default -> null;
-    };
-
-    provider = (providerList != null)
-            ? providerList.stream()
-                .map(bu -> StringUtils.upperCase(
-                        StringUtils.trim(
-                                StringUtils.isNotBlank(CioGroup.getBuName(bu)) 
-                                ? CioGroup.getBuName(bu) 
-                                : bu)))
-                .collect(Collectors.joining(","))
-            : StringUtils.isNotBlank(CioGroup.getBuName(filter.getProvider()))
-                ? CioGroup.getBuName(filter.getProvider()) 
-                : filter.getProvider();
-}
-
-
