62c6b6cffc02608e2f120fed873685e4d5aa4d05
Swagat Mishra <swagatkumarmishra888@gmail.com>
2025-03-10 13:46:49 +0530

    Update promise-race-example.js

--- promise/promise-race-example.js
+++ promise/promise-race-example.js
@@ -19,3 +19,34 @@ Promise.race([
 }).catch((error) => {
     console.log('An error occurred', error);
 })
+private SBOMAssetSummaryResponseDto getSBOMAssetSummaryResponseDto(SBOMAssetSummaryDto sbomAssetSummaryData) {
+    SBOMAssetSummaryResponseDto sbomAssetSummaryDto = new SBOMAssetSummaryResponseDto();
+
+    sbomAssetSummaryDto.setTotalLibraries(Long.valueOf(sbomAssetSummaryData.getAssetLibrary().getLibraryCount()));
+    sbomAssetSummaryDto.setTotalRepositories(Long.valueOf(sbomAssetSummaryData.getAssetRepo().getRepoCount()));
+
+    // Transform data using helper methods
+    List<SBOMAssetTechnologySummaryResponseDto> popularTechnologies = mapTechnologies(sbomAssetSummaryData.getPopularTechnologies());
+    List<SBOMAssetTechnologySummaryResponseDto> outdatedTechnologies = mapTechnologies(sbomAssetSummaryData.getOutDatedTechnologies());
+    List<SBOMHealthSummaryResponseDto> healthSummaries = mapHealthSummaries(sbomAssetSummaryData.getAssetHealth());
+
+    sbomAssetSummaryDto.setPopularTechnologies(popularTechnologies);
+    sbomAssetSummaryDto.setOutDatedTechnologies(outdatedTechnologies);
+    sbomAssetSummaryDto.setAssetHealth(healthSummaries);
+
+    return sbomAssetSummaryDto;
+}
+
+// Helper method to map technologies
+private List<SBOMAssetTechnologySummaryResponseDto> mapTechnologies(List<Technology> technologies) {
+    return technologies.stream()
+            .map(tech -> new SBOMAssetTechnologySummaryResponseDto(tech.getTechnologyName(), tech.getTechnologyCount()))
+            .collect(Collectors.toList());
+}
+
+// Helper method to map health summaries
+private List<SBOMHealthSummaryResponseDto> mapHealthSummaries(List<Health> healthList) {
+    return healthList.stream()
+            .map(health -> new SBOMHealthSummaryResponseDto(Boolean.valueOf(health.getHealthStatus()), health.getHealthCount()))
+            .collect(Collectors.toList());
+}
